f4e533d37961800ad82c9b5cd8387efe2649caaf
matwende <52256025+matwende@users.noreply.github.com>
2020-11-27 09:54:47 +0100

    Zamiana kolejności imienia i nazwiska

--- main.c
+++ main.c
@@ -2,16 +2,10 @@
 
 int main()
 {
-    int i = 1;
-    float f = 1.5;
-    double d = 2.5;
-    char c = 'c';
     
-    printf("Wartosci zmiennych:i=%i, f=%.2f, d=%.2lf, c=%c \n", i, f, d, c);
-    scanf("%i", &i); 
-    scanf("%f", &f);
-    scanf("%lf", &d);
-    scanf(" %c", &c);
-    printf("\nWartosci zmiennych:i=%i, f=%.2f, d=%.2lf, c=%c", i, f, d, c);
-    
-}
+char imie[20], nazwisko[20];
+    printf("Podaj imie i nazwisko\n");
+    scanf("%s %s" , &imie, &nazwisko);
+    printf("%s %s", nazwisko, imie);
+
+} 
